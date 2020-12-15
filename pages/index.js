9a29ea45d747ea5b770c63f9ce93e3922b0df537
AntonPodash <apodash01@gmail.com>
2020-12-15 22:52:39 +0200

    1post

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,8 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
+          <a href="/posts/first-post" className="card">
+            <h3>First Post &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
